83055a88a26702e94ffb4fd5eb373105ea150398
Mᴀxx ⚡ <84794903+MaxxCoderZ@users.noreply.github.com>
2021-06-01 08:51:25 +0530

    Update script.py

--- script.py
+++ script.py
@@ -6,7 +6,7 @@ I'm A simple Zee5 link downloader bot With Permanent Thumbnail Support💯.
 
 Please send me any Zee5 link, I can upload it to telegram as File/Video.
 
-Currently I'm in beta mode 🥺, If found any bugs, report @NexonHex !
+Currently I'm in beta mode 🥺, If found any bugs, report @CoderzSupport !
 
 Click <i>/help</i> for more details....</b>"""
 
@@ -23,13 +23,13 @@ NOTE: Download may take some time! So please wait for it to complete!"""
 
     ABOUT_TEXT = """⭕️<b>My Name :Nexon's Zee5 DL</b>
 
-⭕️<b>Creater :</b> @HexRidded
+⭕️<b>Creater :</b> @MaxxCoderZ
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
 ⭕️<b>Library :</b> <a href='https://docs.pyrogram.org/'>Pyrogram 1.0.7</a> 
 
-⭕️<b>Source Code :</b> 👉 <a href='https://github.com/Hexridder/Zee5-Downloader'>Click Here</a>"""
+⭕️<b>Source Code :</b> 👉 <a href='https://github.com/TrojanzHEX/Zee5-Downloader'>Click Here</a>"""
 
 
 
@@ -41,11 +41,11 @@ NOTE: Download may take some time! So please wait for it to complete!"""
 <i>NOTE : Taking high resolutions may result in files above 2GB and hence cannot Upload to TG. So better select a medium resolution.</i> 😇
 """    
     
-    UPGRADE_TEXT = "PING at @NexonHex_grp"
+    UPGRADE_TEXT = "PING at @CoderzRobot"
     
     DOWNLOAD_START = "Trying to download to my server. This may take a while 😴"
     
-    UPLOAD_START = "Uploading Now ⬆️"
+    UPLOAD_START = "Uploading Now..."
     
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.95GB due to Telegram API limitations."
 
@@ -55,11 +55,11 @@ NOTE: Download may take some time! So please wait for it to complete!"""
     
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom Thumbnail cleared succesfully."
     
-    SHOW_THUMB = "@TroJanzHEX\n\nUse /delthumb to clear this thumbnail."
+    SHOW_THUMB = "@CoderzHEX\n\nUse /delthumb to clear this thumbnail."
     
     NO_THUMB = "SED😕 No saved thumbnails Found!!"
     
-    CUSTOM_CAPTION_UL_FILE = "<b>{newname}\n\n©️ @NexonHex</b>"
+    CUSTOM_CAPTION_UL_FILE = "<b>{newname}\n\n©️ @CoderzHEX | Zee5 DL</b>"
     
     TIMEOUT = "<b><i>Sorry for the delay. It'll help reduce the flood wait</i> 😇\n\nWait for {} sec and try again.</b>"
     
